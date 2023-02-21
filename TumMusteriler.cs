ac2ce088e6a0df2af35a216fd6f880ca3d1d1c40
Emre Can Aşık <93338158+heisanengineer@users.noreply.github.com>
2023-02-21 12:48:25 +0000

    first commit

---
+++ TumMusteriler.cs
@@ -0,0 +1,15 @@
+public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new NorthwindContext())
+            { 
+
+                var customers = db.Customers.ToList();
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine(customer.FirstName + " " + customer.LastName);
+                }
+        }
+        }}
